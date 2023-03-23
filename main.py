aa5c4c24d0f688fd810791d50558bd6d2da6d83c
Krzysztof Nowak <krzysztofn1993@gmail.com>
2023-03-23 21:54:18 +0100

    layer dense object without activation function

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
 import numpy as np
+np.random.seed(0)
+X = [[1, 2, 3, 2.5],
+    [2.0, 5.0, -1.0, 2.0],
+    [-1.5, 2.7, 3.3, -0.8]]
 
-inputs = [[1, 2, 3, 2.5],
-          [2.0, 5.0, -1.0, 2.0],
-          [-1.5, 2.7, 3.3, -0.8]]
 
-weights = [
-    [0.2, 0.8, -0.5, 1.0],
-    [0.5, -0.91, 0.26, -0.5],
-    [-0.26, -0.27, 0.17, 0.87]
-]
+class Layer_Dense:
+    def __init__(self,n_inputs, n_neurons) -> None:
+        self.weights = np.random.randn(n_inputs, n_neurons) * 0.10
+        self.biases = np.zeros((1, n_neurons))
 
-biases = [2,3,0.5]
 
-weights2 = [
-    [0.1, -0.14, 0.5],
-    [-0.5, 0.12, -0.33],
-    [-0.44, 0.73, -0.13]
-]
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
 
-biases2 = [-1, 2, -0.5]
 
-layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
-layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
+layer1 = Layer_Dense(4, 5)
+layer2 = Layer_Dense(5, 2)
 
-print(layer1_outputs)
-print(layer2_outputs)
+layer1.forward(X)
+layer2.forward(layer1.output)
+
+# print(layer1.output)
+print(layer2.output)
