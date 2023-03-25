73b45f4a367671ac1c36ed60b93e70b81b5004af
Krzysztof Nowak <krzysztofn1993@gmail.com>
2023-03-25 12:35:38 +0100

    relu

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import numpy as np
-np.random.seed(0)
+import nnfs
+from nnfs.datasets import spiral_data
+
+nnfs.init()
+
+
 X = [[1, 2, 3, 2.5],
     [2.0, 5.0, -1.0, 2.0],
     [-1.5, 2.7, 3.3, -0.8]]
 
+X, y = spiral_data(100, 3)
 
-class Layer_Dense:
+inputs = [ 0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
+output = []
+
+
+class Layer:
     def __init__(self,n_inputs, n_neurons) -> None:
         self.weights = np.random.randn(n_inputs, n_neurons) * 0.10
         self.biases = np.zeros((1, n_neurons))
@@ -15,11 +25,16 @@ class Layer_Dense:
         self.output = np.dot(inputs, self.weights) + self.biases
 
 
-layer1 = Layer_Dense(4, 5)
-layer2 = Layer_Dense(5, 2)
+class ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+
+layer1 = Layer(2, 5)
+activation1 = ReLU()
 
 layer1.forward(X)
-layer2.forward(layer1.output)
 
-# print(layer1.output)
-print(layer2.output)
+
+activation1.forward(layer1.output)
+print(activation1.output)
